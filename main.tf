ef15248cab7ccfcde101fa5f796a4ec4de6a6444
brajeshkumarDeveloper <brajeshkewat8349@gmail.com>
2026-02-09 22:31:33 +0530

    corrected files

--- main.tf
+++ main.tf
@@ -68,14 +68,14 @@ resource "aws_s3_bucket_website_configuration" "mywebapp" {
 resource "aws_s3_object" "index_html" {
   bucket       = aws_s3_bucket.mywebapp-bucket.bucket
   key          = "index.html"
-  source       = "./index.html"
+  source       = "index.html"
   content_type = "text/html"
 }
 
 resource "aws_s3_object" "styles_css" {
   bucket       = aws_s3_bucket.mywebapp-bucket.bucket
   key          = "styles.css"
-  source       = "./styles.css"
+  source       = "styles.css"
   content_type = "text/css"
 }
 
